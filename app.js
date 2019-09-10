55ccb5c5bb667b0e2176913ad8354fd08e7021d7
Nathan Roy <nathanmroy@yahoo.com>
2019-09-09 20:26:02 -0700

    got json data to display

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const readFileJSON = async (file) => {
+	const data = await readFile(file);
+	return JSON.parse(data);
+};
+
+const readFile = (file) => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(data.toString());
+			}
+		});
+	});
+};
+
+const server = http.createServer(async (req, res) => {
 	try {
-		res.write('hello world');
-		res.end();
+		if (req.url === '/') {
+			res.write('hello world');
+			res.end();
+		} else if (req.url === '/api/guests') {
+			const guests = await readFileJSON('./guests.json');
+			res.write(JSON.stringify(guests));
+			res.end();
+		}
 	} catch (ex) {
 		res.statusCode = 500;
 		res.write(ex.message);
